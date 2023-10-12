7d2134bf9c3cc1d5713f10459c1d509459787075
Dunc1995 <duncanbailey1995@gmail.com>
2023-10-13 09:03:45 +1300

    updates work experience component

--- client/src/lib/WorkExperience.svelte
+++ client/src/lib/WorkExperience.svelte
@@ -1,22 +1,25 @@
 <script lang='ts'>
-  import { WorkExperience } from "../schemas/work-experience";
-
-  const getContent = async () : Promise<WorkExperience> => {
-    const response = await fetch("http://localhost:5187/api/WorkExperience/Get/1");
-    const content = await response.json();
-
-    return content;
-  }
+    import { WorkExperience } from "../schemas/work-experience";
+  
+    const getWorkExperience = async () : Promise<WorkExperience[]> => {
+      const response = await fetch("http://localhost:5187/api/WorkExperience");
+      const content = await response.json();
+  
+      return content;
+    }
 </script>
-{#await getContent() then { jobTitle, startDateDisplay, endDateDisplay, responsibilities }}
-  <div>
+{#await getWorkExperience() then experiences}
+{#each experiences as { jobTitle, startDateDisplay, endDateDisplay, responsibilities }}
+<div>
     <strong>{jobTitle}</strong>
     <br>
     <span>{startDateDisplay} - {endDateDisplay}</span>
     <ul>
-      {#each responsibilities as responsibility}
-        <li>{responsibility.description}</li>
-      {/each}
+        {#each responsibilities as { description }}
+        <li>{description}</li>
+        {/each}
     </ul>
-  </div>
+</div>
+{/each}
 {/await}
+  
